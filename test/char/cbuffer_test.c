9a47d7ad81702ce02275487da91a4533f3bfed94
Felix Koppe <fkoppe@web.de>
2023-11-25 13:43:33 +0100

    fix cbuffer_test

--- test/char/cbuffer_test.c
+++ test/char/cbuffer_test.c
@@ -10,10 +10,10 @@ void sprint(char* const buffer, const size_t count, const char* const format, ..
 
 int main()
 {
-    DARK_TEST_EQ_U(14, dark_cbuffer_snprintf(NULL, 0, "hello %s %jd", "world", 11));
+    DARK_TEST_EQ_U(14, dark_cbuffer_snprintf(NULL, 0, "hello %s %i", "world", 11));
 
     char buffer[14+1]; //additional character \n
-    sprint(buffer, 14+1, "hello %s %jd", "world", 11);
+    sprint(buffer, 14+1, "hello %s %i", "world", 11);
 
     DARK_TEST_EQ_S("hello world 11", buffer, 14+1);
 
