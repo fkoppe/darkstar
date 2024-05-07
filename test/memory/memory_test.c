543ac3492cb4fb88bc84489c284b1ad088059da3
Felix Koppe <fkoppe@web.de>
2024-05-07 13:53:27 +0200

    Error search

--- test/memory/memory_test.c
+++ test/memory/memory_test.c
@@ -40,7 +40,7 @@ int main()
         char* data = dark_malloc(DARK_ALLOCATOR_NATIVE, 5);
         DARK_ASSERT(NULL != data, DARK_ERROR_ALLOCATION);
 
-        dark_realloc(DARK_ALLOCATOR_NATIVE, data, 5, 6);
+        //dark_realloc(DARK_ALLOCATOR_NATIVE, data, 5, 6);
 
         data[0] = 'a';
         data[1] = 'b';
