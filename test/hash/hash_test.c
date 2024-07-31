58d0b21398c0daebe6e568552fde847c9975a206
Felix Koppe <fkoppe@web.de>
2024-07-31 10:25:42 +0200

    Fix error

--- test/hash/hash_test.c
+++ test/hash/hash_test.c
@@ -17,8 +17,8 @@ int main()
         hash_1 = dark_hash_djb2_32(7, arr);
         hash_2 = dark_hash_djb2_32(7, arr);
         hash_3 = dark_hash_djb2_32(6, arr);
-        DARK_TEST_EQ_M(&hash_1, &hash_2, sizeof(uint32_t));
-        DARK_TEST_NE_M(&hash_2, &hash_3, sizeof(uint32_t));
+        DARK_TEST_EQ_U(hash_1, hash_2);
+        DARK_TEST_NE_U(hash_2, hash_3);
     }
     //--------------------------
 
@@ -34,8 +34,8 @@ int main()
         hash_1 = dark_hash_fnv1_32(7, arr);
         hash_2 = dark_hash_fnv1_32(7, arr);
         hash_3 = dark_hash_fnv1_32(6, arr);
-        DARK_TEST_EQ_M(&hash_1, &hash_2, sizeof(uint32_t));
-        DARK_TEST_NE_M(&hash_2, &hash_3, sizeof(uint32_t));
+        DARK_TEST_EQ_U(hash_1, hash_2);
+        DARK_TEST_NE_U(hash_2, hash_3);
     }
     //--------------------------
 
@@ -51,8 +51,8 @@ int main()
         hash_1 = dark_hash_fnv1a_32(7, arr);
         hash_2 = dark_hash_fnv1a_32(7, arr);
         hash_3 = dark_hash_fnv1a_32(6, arr);
-        DARK_TEST_EQ_M(&hash_1, &hash_2, sizeof(uint32_t));
-        DARK_TEST_NE_M(&hash_2, &hash_3, sizeof(uint32_t));
+        DARK_TEST_EQ_U(hash_1, hash_2);
+        DARK_TEST_NE_U(hash_2, hash_3);
     }
     //--------------------------
 
@@ -68,8 +68,8 @@ int main()
         hash_1 = dark_hash_fnv1_64(7, arr);
         hash_2 = dark_hash_fnv1_64(7, arr);
         hash_3 = dark_hash_fnv1_64(6, arr);
-        DARK_TEST_EQ_M(&hash_1, &hash_2, sizeof(uint64_t));
-        DARK_TEST_NE_M(&hash_2, &hash_3, sizeof(uint64_t));
+        DARK_TEST_EQ_U(hash_1, hash_2);
+        DARK_TEST_NE_U(hash_2, hash_3);
     }
     //--------------------------
 
@@ -85,8 +85,8 @@ int main()
         hash_1 = dark_hash_fnv1a_64(7, arr);
         hash_2 = dark_hash_fnv1a_64(7, arr);
         hash_3 = dark_hash_fnv1a_64(6, arr);
-        DARK_TEST_EQ_M(&hash_1, &hash_2, sizeof(uint64_t));
-        DARK_TEST_NE_M(&hash_2, &hash_3, sizeof(uint64_t));
+        DARK_TEST_EQ_U(hash_1, hash_2);
+        DARK_TEST_NE_U(hash_2, hash_3);
     }
     //--------------------------
 
@@ -102,8 +102,8 @@ int main()
         hash_1 = dark_hash_murmur2_32(7, arr);
         hash_2 = dark_hash_murmur2_32(7, arr);
         hash_3 = dark_hash_murmur2_32(6, arr);
-        DARK_TEST_EQ_M(&hash_1, &hash_2, sizeof(uint32_t));
-        DARK_TEST_NE_M(&hash_2, &hash_3, sizeof(uint32_t));
+        DARK_TEST_EQ_U(hash_1, hash_2);
+        DARK_TEST_NE_U(hash_2, hash_3);
     }
     //--------------------------
 
@@ -119,8 +119,8 @@ int main()
         hash_1 = dark_hash_murmur2a_32(7, arr);
         hash_2 = dark_hash_murmur2a_32(7, arr);
         hash_3 = dark_hash_murmur2a_32(6, arr);
-        DARK_TEST_EQ_M(&hash_1, &hash_2, sizeof(uint32_t));
-        DARK_TEST_NE_M(&hash_2, &hash_3, sizeof(uint32_t));
+        DARK_TEST_EQ_U(hash_1, hash_2);
+        DARK_TEST_NE_U(hash_2, hash_3);
     }
     //--------------------------
 
@@ -136,8 +136,8 @@ int main()
         hash_1 = dark_hash_murmur2a_64(7, arr);
         hash_2 = dark_hash_murmur2a_64(7, arr);
         hash_3 = dark_hash_murmur2a_64(6, arr);
-        DARK_TEST_EQ_M(&hash_1, &hash_2, sizeof(uint64_t));
-        DARK_TEST_NE_M(&hash_2, &hash_3, sizeof(uint64_t));
+        DARK_TEST_EQ_U(hash_1, hash_2);
+        DARK_TEST_NE_U(hash_2, hash_3);
     }
     //--------------------------
 
@@ -153,8 +153,8 @@ int main()
         hash_1 = dark_hash_murmur3_32(7, arr);
         hash_2 = dark_hash_murmur3_32(7, arr);
         hash_3 = dark_hash_murmur3_32(6, arr);
-        DARK_TEST_EQ_M(&hash_1, &hash_2, sizeof(uint32_t));
-        DARK_TEST_NE_M(&hash_2, &hash_3, sizeof(uint32_t));
+        DARK_TEST_EQ_U(hash_1, hash_2);
+        DARK_TEST_NE_U(hash_2, hash_3);
     }
     //--------------------------
 
@@ -170,8 +170,8 @@ int main()
         hash_1 = dark_hash_murmur2_seed_32(7, arr, 0);
         hash_2 = dark_hash_murmur2_seed_32(7, arr, 0);
         hash_3 = dark_hash_murmur2_seed_32(6, arr, 0);
-        DARK_TEST_EQ_M(&hash_1, &hash_2, sizeof(uint32_t));
-        DARK_TEST_NE_M(&hash_2, &hash_3, sizeof(uint32_t));
+        DARK_TEST_EQ_U(hash_1, hash_2);
+        DARK_TEST_NE_U(hash_2, hash_3);
     }
     //--------------------------
 
@@ -187,8 +187,8 @@ int main()
         hash_1 = dark_hash_murmur2a_seed_32(7, arr, 0);
         hash_2 = dark_hash_murmur2a_seed_32(7, arr, 0);
         hash_3 = dark_hash_murmur2a_seed_32(6, arr, 0);
-        DARK_TEST_EQ_M(&hash_1, &hash_2, sizeof(uint32_t));
-        DARK_TEST_NE_M(&hash_2, &hash_3, sizeof(uint32_t));
+        DARK_TEST_EQ_U(hash_1, hash_2);
+        DARK_TEST_NE_U(hash_2, hash_3);
     }
     //--------------------------
 
@@ -204,8 +204,8 @@ int main()
         hash_1 = dark_hash_murmur2a_seed_64(7, arr, 0);
         hash_2 = dark_hash_murmur2a_seed_64(7, arr, 0);
         hash_3 = dark_hash_murmur2a_seed_64(6, arr, 0);
-        DARK_TEST_EQ_M(&hash_1, &hash_2, sizeof(uint64_t));
-        DARK_TEST_NE_M(&hash_2, &hash_3, sizeof(uint64_t));
+        DARK_TEST_EQ_U(hash_1, hash_2);
+        DARK_TEST_NE_U(hash_2, hash_3);
     }
     //--------------------------
 
@@ -221,8 +221,8 @@ int main()
         hash_1 = dark_hash_murmur3_seed_32(7, arr, 0);
         hash_2 = dark_hash_murmur3_seed_32(7, arr, 0);
         hash_3 = dark_hash_murmur3_seed_32(6, arr, 0);
-        DARK_TEST_EQ_M(&hash_1, &hash_2, sizeof(uint32_t));
-        DARK_TEST_NE_M(&hash_2, &hash_3, sizeof(uint32_t));
+        DARK_TEST_EQ_U(hash_1, hash_2);
+        DARK_TEST_NE_U(hash_2, hash_3);
     }
     //--------------------------
 
@@ -238,8 +238,8 @@ int main()
         hash_1 = dark_hash_superfast_32(7, arr);
         hash_2 = dark_hash_superfast_32(7, arr);
         hash_3 = dark_hash_superfast_32(6, arr);
-        DARK_TEST_EQ_M(&hash_1, &hash_2, sizeof(uint32_t));
-        DARK_TEST_NE_M(&hash_2, &hash_3, sizeof(uint32_t));
+        DARK_TEST_EQ_U(hash_1, hash_2);
+        DARK_TEST_NE_U(hash_2, hash_3);
     }
     //--------------------------
 
