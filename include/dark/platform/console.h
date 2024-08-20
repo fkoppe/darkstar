4ed9437e0fb0820381f15801b218f744b256f66b
Felix Koppe <fkoppe@web.de>
2024-08-20 20:27:34 +0200

    Make console colors be macros

--- include/dark/platform/console.h
+++ include/dark/platform/console.h
@@ -26,85 +26,85 @@
 #include <dark/core/std.h>
 
 #if !defined(___DARK_CONSOLE_NOCOLOR)
-static const char* const DARK_CONSOLE_COLOR_FG_RED = "\033[31m";
-static const char* const DARK_CONSOLE_COLOR_FG_GREEN = "\033[32m";
-static const char* const DARK_CONSOLE_COLOR_FG_YELLOW = "\033[33m";
-static const char* const DARK_CONSOLE_COLOR_FG_BLUE = "\033[34m";
-static const char* const DARK_CONSOLE_COLOR_FG_MAGENTA = "\033[35m";
-static const char* const DARK_CONSOLE_COLOR_FG_CYAN = "\033[36m";
+#define DARK_CONSOLE_COLOR_FG_RED "\033[31m"
+#define DARK_CONSOLE_COLOR_FG_GREEN "\033[32m"
+#define DARK_CONSOLE_COLOR_FG_YELLOW "\033[33m"
+#define DARK_CONSOLE_COLOR_FG_BLUE "\033[34m"
+#define DARK_CONSOLE_COLOR_FG_MAGENTA "\033[35m"
+#define DARK_CONSOLE_COLOR_FG_CYAN "\033[36m"
 
-static const char* const DARK_CONSOLE_COLOR_BFG_RED = "\033[91m";
-static const char* const DARK_CONSOLE_COLOR_BFG_GREEN = "\033[92m";
-static const char* const DARK_CONSOLE_COLOR_BFG_YELLOW = "\033[93m";
-static const char* const DARK_CONSOLE_COLOR_BFG_BLUE = "\033[94m";
-static const char* const DARK_CONSOLE_COLOR_BFG_MAGENTA = "\033[95m";
-static const char* const DARK_CONSOLE_COLOR_BFG_CYAN = "\033[96m";
+#define DARK_CONSOLE_COLOR_BFG_RED "\033[91m"
+#define DARK_CONSOLE_COLOR_BFG_GREEN "\033[92m"
+#define DARK_CONSOLE_COLOR_BFG_YELLOW "\033[93m"
+#define DARK_CONSOLE_COLOR_BFG_BLUE "\033[94m"
+#define DARK_CONSOLE_COLOR_BFG_MAGENTA "\033[95m"
+#define DARK_CONSOLE_COLOR_BFG_CYAN "\033[96m"
 
-static const char* const DARK_CONSOLE_COLOR_BG_RED = "\033[41m";
-static const char* const DARK_CONSOLE_COLOR_BG_GREEN = "\033[42m";
-static const char* const DARK_CONSOLE_COLOR_BG_YELLOW = "\033[43m";
-static const char* const DARK_CONSOLE_COLOR_BG_BLUE = "\033[44m";
-static const char* const DARK_CONSOLE_COLOR_BG_MAGENTA = "\033[45m";
-static const char* const DARK_CONSOLE_COLOR_BG_CYAN = "\033[46m";
+#define DARK_CONSOLE_COLOR_BG_RED "\033[41m"
+#define DARK_CONSOLE_COLOR_BG_GREEN "\033[42m"
+#define DARK_CONSOLE_COLOR_BG_YELLOW "\033[43m"
+#define DARK_CONSOLE_COLOR_BG_BLUE "\033[44m"
+#define DARK_CONSOLE_COLOR_BG_MAGENTA "\033[45m"
+#define DARK_CONSOLE_COLOR_BG_CYAN "\033[46m"
 
-static const char* const DARK_CONSOLE_COLOR_BBG_RED = "\033[101m";
-static const char* const DARK_CONSOLE_COLOR_BBG_GREEN = "\033[102m";
-static const char* const DARK_CONSOLE_COLOR_BBG_YELLOW = "\033[103m";
-static const char* const DARK_CONSOLE_COLOR_BBG_BLUE = "\033[104m";
-static const char* const DARK_CONSOLE_COLOR_BBG_MAGENTA = "\033[105m";
-static const char* const DARK_CONSOLE_COLOR_BBG_CYAN = "\033[106m";
+#define DARK_CONSOLE_COLOR_BBG_RED "\033[101m"
+#define DARK_CONSOLE_COLOR_BBG_GREEN "\033[102m"
+#define DARK_CONSOLE_COLOR_BBG_YELLOW "\033[103m"
+#define DARK_CONSOLE_COLOR_BBG_BLUE "\033[104m"
+#define DARK_CONSOLE_COLOR_BBG_MAGENTA "\033[105m"
+#define DARK_CONSOLE_COLOR_BBG_CYAN "\033[106m"
 
-static const char* const DARK_CONSOLE_COLOR_EFG_RED = "\033[38;5;160m";
-static const char* const DARK_CONSOLE_COLOR_EFG_ORANGE = "\033[38;5;202m";
-static const char* const DARK_CONSOLE_COLOR_EFG_GREEN = "\033[38;5;154m";
-static const char* const DARK_CONSOLE_COLOR_EFG_BLUE = "\033[38;5;159m";
+#define DARK_CONSOLE_COLOR_EFG_RED "\033[38;5;160m"
+#define DARK_CONSOLE_COLOR_EFG_ORANGE "\033[38;5;202m"
+#define DARK_CONSOLE_COLOR_EFG_GREEN "\033[38;5;154m"
+#define DARK_CONSOLE_COLOR_EFG_BLUE "\033[38;5;159m"
 
-static const char* const DARK_CONSOLE_COLOR_EBG_RED = "\033[48;5;160m";
-static const char* const DARK_CONSOLE_COLOR_EBG_ORANGE = "\033[48;5;202m";
-static const char* const DARK_CONSOLE_COLOR_EBG_GREEN = "\033[48;5;154m";
-static const char* const DARK_CONSOLE_COLOR_EBG_BLUE = "\033[48;5;159m";
+#define DARK_CONSOLE_COLOR_EBG_RED "\033[48;5;160m"
+#define DARK_CONSOLE_COLOR_EBG_ORANGE "\033[48;5;202m"
+#define DARK_CONSOLE_COLOR_EBG_GREEN "\033[48;5;154m"
+#define DARK_CONSOLE_COLOR_EBG_BLUE "\033[48;5;159m"
 
-static const char* const DARK_CONSOLE_COLOR_RESET = "\033[0m";
+#define DARK_CONSOLE_COLOR_RESET "\033[0m"
 #else
-static const char* const DARK_CONSOLE_COLOR_FG_RED = "";
-static const char* const DARK_CONSOLE_COLOR_FG_GREEN = "";
-static const char* const DARK_CONSOLE_COLOR_FG_YELLOW = "";
-static const char* const DARK_CONSOLE_COLOR_FG_BLUE = "";
-static const char* const DARK_CONSOLE_COLOR_FG_MAGENTA = "";
-static const char* const DARK_CONSOLE_COLOR_FG_CYAN = "";
+#define DARK_CONSOLE_COLOR_FG_RED ""
+#define DARK_CONSOLE_COLOR_FG_GREEN ""
+#define DARK_CONSOLE_COLOR_FG_YELLOW ""
+#define DARK_CONSOLE_COLOR_FG_BLUE ""
+#define DARK_CONSOLE_COLOR_FG_MAGENTA ""
+#define DARK_CONSOLE_COLOR_FG_CYAN ""
 
-static const char* const DARK_CONSOLE_COLOR_BFG_RED = "";
-static const char* const DARK_CONSOLE_COLOR_BFG_GREEN = "";
-static const char* const DARK_CONSOLE_COLOR_BFG_YELLOW = "";
-static const char* const DARK_CONSOLE_COLOR_BFG_BLUE = "";
-static const char* const DARK_CONSOLE_COLOR_BFG_MAGENTA = "";
-static const char* const DARK_CONSOLE_COLOR_BFG_CYAN = "";
+#define DARK_CONSOLE_COLOR_BFG_RED ""
+#define DARK_CONSOLE_COLOR_BFG_GREEN ""
+#define DARK_CONSOLE_COLOR_BFG_YELLOW ""
+#define DARK_CONSOLE_COLOR_BFG_BLUE ""
+#define DARK_CONSOLE_COLOR_BFG_MAGENTA ""
+#define DARK_CONSOLE_COLOR_BFG_CYAN ""
 
-static const char* const DARK_CONSOLE_COLOR_BG_RED = "";
-static const char* const DARK_CONSOLE_COLOR_BG_GREEN = "";
-static const char* const DARK_CONSOLE_COLOR_BG_YELLOW = "";
-static const char* const DARK_CONSOLE_COLOR_BG_BLUE = "";
-static const char* const DARK_CONSOLE_COLOR_BG_MAGENTA = "";
-static const char* const DARK_CONSOLE_COLOR_BG_CYAN = "";
+#define DARK_CONSOLE_COLOR_BG_RED ""
+#define DARK_CONSOLE_COLOR_BG_GREEN ""
+#define DARK_CONSOLE_COLOR_BG_YELLOW ""
+#define DARK_CONSOLE_COLOR_BG_BLUE ""
+#define DARK_CONSOLE_COLOR_BG_MAGENTA ""
+#define DARK_CONSOLE_COLOR_BG_CYAN ""
 
-static const char* const DARK_CONSOLE_COLOR_BBG_RED = "";
-static const char* const DARK_CONSOLE_COLOR_BBG_GREEN = "";
-static const char* const DARK_CONSOLE_COLOR_BBG_YELLOW = "";
-static const char* const DARK_CONSOLE_COLOR_BBG_BLUE = "";
-static const char* const DARK_CONSOLE_COLOR_BBG_MAGENTA = "";
-static const char* const DARK_CONSOLE_COLOR_BBG_CYAN = "";
+#define DARK_CONSOLE_COLOR_BBG_RED ""
+#define DARK_CONSOLE_COLOR_BBG_GREEN ""
+#define DARK_CONSOLE_COLOR_BBG_YELLOW ""
+#define DARK_CONSOLE_COLOR_BBG_BLUE ""
+#define DARK_CONSOLE_COLOR_BBG_MAGENTA ""
+#define DARK_CONSOLE_COLOR_BBG_CYAN ""
 
-static const char* const DARK_CONSOLE_COLOR_EFG_RED = "";
-static const char* const DARK_CONSOLE_COLOR_EFG_ORANGE = "";
-static const char* const DARK_CONSOLE_COLOR_EFG_GREEN = "";
-static const char* const DARK_CONSOLE_COLOR_EFG_BLUE = "";
+#define DARK_CONSOLE_COLOR_EFG_RED ""
+#define DARK_CONSOLE_COLOR_EFG_ORANGE ""
+#define DARK_CONSOLE_COLOR_EFG_GREEN ""
+#define DARK_CONSOLE_COLOR_EFG_BLUE ""
 
-static const char* const DARK_CONSOLE_COLOR_EBG_RED = "";
-static const char* const DARK_CONSOLE_COLOR_EBG_ORANGE = "";
-static const char* const DARK_CONSOLE_COLOR_EBG_GREEN = "";
-static const char* const DARK_CONSOLE_COLOR_EBG_BLUE = "";
+#define DARK_CONSOLE_COLOR_EBG_RED ""
+#define DARK_CONSOLE_COLOR_EBG_ORANGE ""
+#define DARK_CONSOLE_COLOR_EBG_GREEN ""
+#define DARK_CONSOLE_COLOR_EBG_BLUE ""
 
-static const char* const DARK_CONSOLE_COLOR_RESET = "";
+#define DARK_CONSOLE_COLOR_RESET ""
 #endif // !defined(___DARK_CONSOLE_NOCOLOR)
 
 void dark_console_hide(void);
